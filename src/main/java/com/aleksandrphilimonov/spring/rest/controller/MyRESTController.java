148d18caecc7d78c15826dae4e6f2a74146c8c63
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-27 18:24:15 +0300

    delete employee

--- src/main/java/com/aleksandrphilimonov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/aleksandrphilimonov/spring/rest/controller/MyRESTController.java
@@ -7,6 +7,7 @@ import com.aleksandrphilimonov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,12 +50,22 @@ public class MyRESTController {
     }
 
     @PutMapping("/employees")
-    public Employee updateEmployee(@RequestBody Employee employee){
+    public Employee updateEmployee(@RequestBody Employee employee) {
 
         employeeService.saveEmployee(employee);
 
         return employee;
     }
 
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in DataBase");
+        }
+        employeeService.deleteEmployee(id);
+        return "Employee with id: " + id + "was deleted.";
+    }
 
 }
