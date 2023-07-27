8b102f0166c6cf28934d60d525b482c936beb377
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-27 18:19:15 +0300

    update employee

--- src/main/java/com/aleksandrphilimonov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/aleksandrphilimonov/spring/rest/controller/MyRESTController.java
@@ -10,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +39,22 @@ public class MyRESTController {
         }
         return employee;
     }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return employee;
+
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
 
 }
